e6ab4fcd40c793b44760db631c565a847f42e2a1
Ermand Duro <ermand.duro@gmail.com>
2014-12-10 22:18:25 +0100

    Add method to get completed attribute as boolean.

--- app/models/Todo.php
+++ app/models/Todo.php
@@ -1,5 +1,17 @@
 <?php
 
 class Todo extends \Eloquent {
-	protected $fillable = [];
+
+	protected $fillable = ['title', 'completed'];
+
+    /**
+     * Get completed attribute as boolean.
+     *
+     * @param $value
+     * @return bool
+     */
+    public function getCompletedAttribute($value)
+    {
+        return (boolean) $value;
+    }
 }
