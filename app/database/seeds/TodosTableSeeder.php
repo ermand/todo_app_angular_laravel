0f1fd3985d923249b6c7d4040df4741528f3dee9
Ermand Duro <ermand.duro@gmail.com>
2014-12-11 10:09:26 +0100

    Change TodosTableSeeder.

--- app/database/seeds/TodosTableSeeder.php
+++ app/database/seeds/TodosTableSeeder.php
@@ -6,12 +6,16 @@ class TodosTableSeeder extends Seeder {
 
 	public function run()
 	{
+        Todo::truncate();
+
 		$faker = Faker::create();
 
-		foreach(range(1, 10) as $index)
+        $todos = ['Go to the store.', 'Exercise more.', 'Read a new book.', 'Lear more Angular JS', 'Read more about DDD.'];
+
+		foreach(range(1, 5) as $index)
 		{
 			Todo::create([
-                'title' => $faker->sentence(),
+                'title' => $faker->unique()->randomElement($todos),
                 'completed' => $faker->boolean()
 			]);
 		}
